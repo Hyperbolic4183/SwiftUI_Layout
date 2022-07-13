79011f5a2a6016a7c1f630749fe0fc772c6c6906
OtsukaAmane <otsuka.amane@gmail.com>
2022-07-13 19:40:24 +0900

    Initial Commit

---
+++ SwiftUI_Layout/SwiftUI_LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LayoutApp.swift
+//  SwiftUI_Layout
+//
+//  Created by 大塚 周 on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
