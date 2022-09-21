ada47504f33ee8f6b725cfbe7795ed1897f2d2c9
OtsukaAmane <otsuka.amane@gmail.com>
2022-09-21 20:12:06 +0900

    001を完了

--- SwiftUI_Layout/001/Layout_001.swift
+++ SwiftUI_Layout/001/Layout_001.swift
@@ -5,4 +5,35 @@
 //  Created by 大塚 周 on 2022/09/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct Layout001: View {
+    let str: String = "Hello, world!"
+    var body: some View {
+        VStack {
+            HStack {
+                Text(str)
+                    .layoutPriority(1)
+                Spacer()
+                Text(str)
+            }
+            Spacer()
+            Text(str)
+            Spacer()
+            HStack {
+                Text(str)
+                Spacer()
+                Text(str)
+                    .layoutPriority(1)
+            }
+        }
+        .padding()
+    }
+}
+
+struct Layout001_Previews: PreviewProvider {
+    static var previews: some View {
+        Layout001()
+            .previewLayout(.fixed(width: 200, height: 200))
+    }
+}
